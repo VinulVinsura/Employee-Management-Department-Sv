760c9d0e41be0a6701b54e90b08016442caf0b21
Vinul Vinsura <vinulviksura16@gmail.com>
2024-09-16 23:03:34 +0530

    first commit

---
+++ src/main/java/com/example/employeemanagementdepartmentsv/EmployeeManagementDepartmentSvApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementdepartmentsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementDepartmentSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementDepartmentSvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeemanagementdepartmentsv/EmployeeManagementDepartmentSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementdepartmentsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementDepartmentSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
